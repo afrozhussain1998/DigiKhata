29f842852ba2abf2183bf356c24e27d5e826063e
afrozhussain1998 <afrozhussain1998@gmail.com>
2026-03-01 23:37:34 +0530

    Productuction Ready

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:5000/api';
+// const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://digikhata-backend.onrender.com/api';
 
 const api = {
     // Utility for standard requests
